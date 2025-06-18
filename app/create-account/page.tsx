8e1d3cd40220470b68f87dedc84d7e1c68d9c268
Your Name <ayush.0.kumar2224@gmail.com>
2025-06-18 18:27:32 +0530

    add Create Account and Forgot Password pages
    - Created the Create Account page with form inputs for email, name, and
    password
    - Implemented the Forgot Password page with input for recovery email
    - Integrated basic form validation and loading states
    - Added navigation links between login, create account, and forgot
    password pages
    - Ensured consistent styling using shared UI components and Tailwind
    classes

---
+++ app/create-account/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { CreateAccountForm } from "./CreateAccountForm";
+
+const CreateAccountPage = () => {
+  return (
+    <div className="flex items-center justify-center h-minus-135 ">
+      <CreateAccountForm />
+    </div>
+  );
+};
+
+export default CreateAccountPage;
---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { ForgotPasswordForm } from "./ForgotPasswordForm";
+
+const ForgotPasswordPage = () => {
+  return (
+    <div className="flex items-center justify-center h-minus-135">
+      <ForgotPasswordForm />
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
